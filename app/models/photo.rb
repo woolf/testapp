709dc54087a92fea6cac9591c23fd1e6faa1879b
Sergiy Volkov <wolf@sergiymacbook.int>
2014-09-16 02:02:26 +0300

    save and display photos

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,6 +3,6 @@ class Photo < ActiveRecord::Base
   belongs_to :collection
   belongs_to :user
 
-  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :original => "750x500>", :medium => "555x317>", :thumb => "263x158>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
